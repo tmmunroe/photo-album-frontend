ca55d830b589df26e2b5ef1a894a6cb7189a08ee
Turner Mandeville <t.m.munroe@gmail.com>
2022-11-11 02:10:46 -0500

    use web has history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory, RouterLink } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory, RouterLink } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
