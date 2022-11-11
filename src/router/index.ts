d1ba7ce00b709013b3a824dba26dba04f5435d08
Turner Mandeville <t.m.munroe@gmail.com>
2022-11-11 02:53:26 -0500

    try web hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory, RouterLink } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory, RouterLink } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
